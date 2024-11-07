688c2bc6bdcfb0adff60c4fd39543a7507c629bc
Viktoriya Diukina <viki_dyukina@mail.ru>
2024-11-07 10:09:09 +0400

    created common directory, user_group module

---
+++ common/bot_cmds_list.py
@@ -0,0 +1,9 @@
+from aiogram.types import BotCommand
+
+
+private = [
+    BotCommand(command='menu', description='View the menu'),
+    BotCommand(command='about', description='About us'),
+    BotCommand(command='payment', description='Payment options'),
+    BotCommand(command='shipping', description='Delivery options'),
+]
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,7 +1,11 @@
-from aiogram import types, Router
+from aiogram import F, types, Router
 from aiogram.filters import CommandStart, Command
+from aiogram.utils.formatting import Text
+
+from filters.chat_types import ChatTypeFilter
 
 user_private_router = Router()
+user_private_router.message.filter(ChatTypeFilter(['private']))
 
 
 @user_private_router.message(CommandStart())
@@ -9,6 +13,25 @@ async def start_cmd(message: types.Message):
     await message.answer("Hi, I'm a virtual assistant")
 
 
-@user_private_router.message(Command('menu', 'name'))
+# @user_private_router.message(F.text.lower() == "меню")
+@user_private_router.message(Text(equals="menu", ignore_case=True))
 async def menu_cmd(message: types.Message):
     await message.answer("Our menu:")
+
+
+@user_private_router.message(F.text.lower() == "About us")
+@user_private_router.message(Command("about"))
+async def about_cmd(message: types.Message):
+    await message.answer("About us:")
+
+
+@user_private_router.message(F.text.lower() == "Payment options")
+@user_private_router.message(Command("payment"))
+async def payment_cmd(message: types.Message):
+    await message.answer("Our payment options:")
+
+
+@user_private_router.message((F.text.lower().contains('delivery')) | (F.text.lower() == 'Delivery options'))
+@user_private_router.message(Command("shipping"))
+async def menu_cmd(message: types.Message):
+    await message.answer("Delivery options:")
